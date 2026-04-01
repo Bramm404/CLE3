ac7f394564408230d42123ccf330e1b2aaddec38
Bram den Boer <b.h.r.denboer@student.utwente.nl>
2026-04-01 10:01:13 +0200

    Begin gemaakt aan login systeem

---
+++ includes/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$user = "root";
+$pass = "";
+$db = "geldlift_db";
+$host = "localhost";
+
+mysqli_connect($host, $user, $pass, $db);
+
--- register.php
+++ register.php
@@ -1,1 +1,40 @@
 <?php
+/** @var $db */
+session_start();
+
+
+if (isset($_SESSION['login'])) {
+    header('Location: index.php');
+    exit;
+}
+
+if (isset($_POST['submit'])) {
+    require_once 'includes/db.php';
+    $name = mysqli_real_escape_string($db, $_POST['name']);
+    $surname = mysqli_real_escape_string($db, $_POST['surname']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']);
+    $business = mysqli_real_escape_string($db, $_POST['business']);
+    $errors = [];
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hoofdpagina </title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js" defer></script>
+</head>
+<body>
+
+</body>
+
+
+</html>
