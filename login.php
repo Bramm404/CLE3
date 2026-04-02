1ce9d458950605d427f842d10fa8b8cb653ad241
ctrl-yen <shayenner1@gmail.com>
2026-04-02 12:07:35 +0200

    CSS fix
    FIX paths css

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ if(isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Login </title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <script src="js/register.js" defer></script>
 </head>
 <body>
