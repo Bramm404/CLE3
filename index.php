1eb5a42217f9cfba0a0c3bdc19bd99a2bae7af6e
Bram den Boer <b.h.r.denboer@student.utwente.nl>
2026-04-02 12:04:36 +0200

    login gemoved naar dialog box

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
 <?php
+/** @var  $db */
 
+$login = false;
+
+if (isset($_SESSION['login'])) {
+    $login = true;
+}
+
+$errors = [];
+
+if(isset($_POST['submit'])) {
+    require_once 'includes/db.php';
+    $user = mysqli_real_escape_string($db, $_POST['user']);
+    $pass = mysqli_real_escape_string($db, $_POST['password']);
+    require_once 'includes/formvalidator.php';
+
+    if(empty($errors)) {
+        $query = "SELECT * FROM users WHERE email = '$user'";
+        $result = mysqli_query($db, $query);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $client = mysqli_fetch_assoc($result);
+            $hash = $client['password'];
+
+            if (password_verify($pass, $hash)) {
+                $_SESSION['login'] = true;
+                $_SESSION['email'] = $client['email'];
+                $_SESSION['name'] = $client['name'];
+                $_SESSION['surname'] = $client['surname'];
+
+
+                header('Location: index.php');
+                exit;
+            } else {
+                $errors['password'] = "Wachtwoord is onjuist";
+            }
+        } else {
+            $errors['user'] = "Geen gebruiker gevonden";
+        }
+    }
+}
 
 ?>
 
@@ -12,6 +53,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>De Geldige Lift</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js" defer></script>
 </head>
 <body>
 <nav>
@@ -21,7 +63,7 @@
         <a href="product.php">Ons product</a>
         <a href="about.php">Over ons</a>
         <a href="profile.php">Mijn profiel</a>
-        <a href="#" class="log-in-button">Log in</a>
+        <a href="#" class="log-in-button" id="loginBtn">Log in</a>
     </div>
 </nav>
 
@@ -75,6 +117,30 @@
             <button type="submit" class="submit-btn">Versturen</button>
         </form>
     </section>
+
+    <section>
+        <dialog>
+        <div>
+            <form action="" method="post">
+                <div>
+                    <label for="user">E-Mail</label>
+                    <input type="text" name="user" class="field" id="name" value="<?= $user ?? ''?>">
+                        <p class="error"> <?= $errors['user'] ?? ''?></p>
+                </div>
+
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="field" id="password">
+                        <p class="error"> <?= $errors['password'] ?? ''?></p>
+                </div>
+
+                <button type="submit" name="submit">Log in</button>
+
+            </form>
+        </div>
+         </dialog>
+    </sectioN>
+
 </main>
 
 <footer>
--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 window.addEventListener('load', init)
 
+const loginBtn = document.querySelector('#loginBtn')
+const dialog = document.querySelector('dialog')
 
 
 function init() {
-
+    loginBtn.addEventListener('click', () => {
+        dialog.showModal()
+    })
 }
 
