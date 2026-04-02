ff5d26f324f94aa1651f66013e66b6fe908e07b9
ctrl-yen <shayenner1@gmail.com>
2026-04-02 11:23:45 +0200

    ADD all the work from my personal file

--- index.php
+++ index.php
@@ -3,19 +3,93 @@
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="nl">
+<!doctype html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Hoofdpagina </title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Service</title>
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/main.js" defer></script>
 </head>
+<body>
+<nav>
+    <img src="#">
+    <div>
+        <a href="index.php">Home</a>
+        <a href="product.php">Ons product</a>
+        <a href="about.php">Over ons</a>
+        <a href="profile.php">Mijn profiel</a>
+        <a href="#" class="log-in-button">Log in</a>
+    </div>
+</nav>
 
 <main>
+    <header>
+        <!--        <img src="images/leafs.png">-->
+        <h1>Online service</h1>
+    </header>
 
+    <section class="product-preview">
+        <div>
+            <h2>Ons product</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum consequat ligula, vitae bibendum enim vestibulum ac. Ut posuere libero diam, pretium malesuada nibh pharetra in. Phasellus at ante justo. Duis eu nisi non mauris ultrices condimentum. In posuere augue sed metus tincidunt, a vulputate urna sodales. Cras hendrerit ullamcorper lorem, sit amet tincidunt sem luctus in. Sed efficitur nisi non quam ultricies, id mattis sem malesuada. Donec luctus placerat sollicitudin. Phasellus nec neque et leo condimentum auctor commodo nec ligula.</p>
+            <a href="#">Ga naar pagina</a>
+        </div>
 
+        <img src="images/sketch.jpg">
+    </section>
+
+    <section class="form">
+        <h2>Contact</h2>
+        <p>Heeft u een vraag? Neem dan contact met ons op:</p>
+
+        <form>
+            <div class="details">
+                <div class="first-name-last-name">
+                    <label for="firstName">voornaam</label>
+                    <input type="text" id="firstName" name="firstName">
+
+                    <label for="lastName">achternaam</label>
+                    <input type="text" id="lastName" name="lastName">
+                </div>
+
+                <label for="email">emailadres</label>
+                <input type="email" id="email" name="email">
+
+                <div class="address-home-number">
+                    <label for="address">adres en huisnummer</label>
+                    <input type="text" id="address" name="address">
+
+                    <label for="city">postcode en stad</label>
+                    <input type="text" id="city" name="city">
+                </div>
+            </div>
+
+            <div class="question-text-area">
+                <label for="question">uw vraag</label>
+                <textarea id="question" name="question"></textarea>
+            </div>
+
+            <button type="submit" class="submit-btn">Versturen</button>
+        </form>
+    </section>
 </main>
+
+<footer>
+    <div>
+        <a href="#">algemene voorwaarden</a>
+        <a href="#">privacy</a>
+        <a href="#">cookies</a>
+    </div>
+    <div class="icons-copyright">
+        <img src="#">
+        <img src="#">
+        <img src="#">
+
+        <p>@Our Product</p>
+    </div>
+</footer>
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Product</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+    <img src="#">
+    <div>
+        <a href="index.php">Home</a>
+        <a href="product.php">Ons product</a>
+        <a href="about.php">Over ons</a>
+        <a href="profile.php">Mijn profiel</a>
+        <a href="#" class="log-in-button">Log in</a>
+    </div>
+</nav>
+
+<main>
+    <header>
+        <h1>Ons product</h1>
+    </header>
+
+    <img src="#">
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum consequat ligula, vitae bibendum enim vestibulum ac. Ut posuere libero diam, pretium malesuada nibh pharetra in. Phasellus at ante justo. Duis eu nisi non mauris ultrices condimentum. In posuere augue sed metus tincidunt, a vulputate urna sodales. Cras hendrerit ullamcorper lorem, sit amet tincidunt sem luctus in. Sed efficitur nisi non quam ultricies, id mattis sem malesuada. Donec luctus placerat sollicitudin. Phasellus nec neque et leo condimentum auctor commodo nec ligula. Donec nec eros condimentum, dignissim ex in, posuere lacus. Nulla tempus quam at volutpat luctus. Aliquam interdum aliquet lectus, eu commodo est mollis et.</p>
+
+    <p>Sed tempus augue et metus feugiat, ut luctus massa placerat. Vestibulum molestie pretium lobortis. Donec ut ornare leo. Cras eget ornare erat. Vivamus et purus et lacus ornare maximus. Proin ut purus in quam rutrum ultricies. Suspendisse eget purus molestie, dictum ligula at, sodales felis. Morbi in lorem nec tellus molestie ultricies. Ut vestibulum turpis lectus, id iaculis nibh tempor ut. Sed in est sem. Aenean et rhoncus tortor, vulputate pretium odio.</p>
+
+    <p>Maecenas commodo velit nec tristique mattis. Morbi eu arcu purus. Suspendisse pulvinar lacus id urna imperdiet pharetra. In semper rutrum erat, vitae volutpat magna euismod a. Suspendisse accumsan sollicitudin lacus, ac porttitor nisl lobortis in. Vestibulum eros risus, molestie ac eros vitae, vulputate luctus nisi. Aenean vestibulum id augue bibendum malesuada. Suspendisse luctus magna vitae vestibulum consectetur. Phasellus malesuada pulvinar purus. Duis dictum facilisis nisl fringilla fermentum. Proin tincidunt ex sit amet tellus eleifend hendrerit. Praesent vitae tristique nisi. Proin convallis egestas velit, accumsan efficitur libero dapibus in. Maecenas sagittis interdum tellus non molestie. Maecenas ut nibh vitae tortor congue aliquam.</p>
+</main>
+
+<footer>
+    <div>
+        <a href="#">algemene voorwaarden</a>
+        <a href="#">privacy</a>
+        <a href="#">cookies</a>
+    </div>
+    <div class="icons-copyright">
+        <img src="#">
+        <img src="#">
+        <img src="#">
+
+        <p>@Our Product</p>
+    </div>
+</footer>
+</body>
+</html>
